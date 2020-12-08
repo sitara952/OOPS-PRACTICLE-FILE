9a91d0eaf74664d6f195ac295360faffc1fb1e5e
sitara952 <71247658+sitara952@users.noreply.github.com>
2020-12-08 15:53:17 +0530

    Create no.ofwords.cpp

---
+++ no.ofwords.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout<<"Enter string-";
+    gets(str);
+    int len=strlen(str),word=1;
+    for(int i=0;i<len;i++)
+    {
+        if(str[i]== ' '){
+            word++;
+        }
+    }
+    cout<<"No. of words-->"<<word;
+}
